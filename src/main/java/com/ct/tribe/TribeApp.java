4dbfc7b6f9213143ffedc608edf5a6fb8eb7a997
CTtao <1838346304@qq.com>
2023-01-23 11:31:38 +0800

    Mapper扫描路径

--- src/main/java/com/ct/tribe/TribeApp.java
+++ src/main/java/com/ct/tribe/TribeApp.java
@@ -1,5 +1,6 @@
 package com.ct.tribe;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @description
  */
 @SpringBootApplication
+@MapperScan("com.ct.tribe.mapper")
 public class TribeApp {
     public static void main(String[] args) {
         SpringApplication.run(TribeApp.class,args);
